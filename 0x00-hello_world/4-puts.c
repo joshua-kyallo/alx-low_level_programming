c47d05773806d5119edfab1ce257478dbc612136
joshua-kyallo <Kyalllohjoshua@gmail.com>
2023-03-16 13:01:18 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main (void)
+/**
+ * main - this is the main function
+ *
+ * Return: always 0
+ */
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
