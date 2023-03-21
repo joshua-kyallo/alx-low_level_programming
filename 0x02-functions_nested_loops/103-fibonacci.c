bb3d27d2d436ef2077e942e29c30f0cbb0821ca6
joshua-kyallo <Kyalllohjoshua@gmail.com>
2023-03-21 13:17:25 -0700

    Question fifteen

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 
 /**
- * main - this is the main function
- * less than 4000000.
- * Return: 0 when successfull
+ * main - prints the sum of even fibonacci numbers
+ * less than 4000000
+ * Return: nothing!
  */
 
 int main(void)
+
 {
 	int i = 0;
 	long j = 1, k = 2, sum = k;
 
-	while (k + j < 4000000)
+	while (k + j < 400000
 	{
-		k += j;
-		if (k % 2 == 0)
-		sum += k;
-		j = k - j;
-		i++;
+	k += j;
+	if (k % 2 == 0)
+	sum += k;
+	j = k - j;
+	++i;
 	}
 	printf("%ld\n", sum);
 	return (0);
+}
