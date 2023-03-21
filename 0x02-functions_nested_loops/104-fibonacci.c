89cbfb096c7683005d44e96ba8a5a104718796c2
joshua-kyallo <kyallohjoshua@gmail.com>
2023-03-22 00:10:55 +0300

    Question sixteen

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,30 +1,35 @@
 #include <stdio.h>
 
 /**
- * main - this is the main function
+ * main - this is the main fuction
+ *
  * Return: 0 when successfull
  */
 
 int main(void)
 {
-	int a = 1;
-	int b = 2;
-	int i, c;
+	int num1 = 1;
+	int num2 = 2;
+	int count = 0;
+	int total = 0;
+	int temp;
 
-	for (i = 1; i <= 98; i++)
+	while (count < 	98)
 	{
-		c = a + b;
-		printf("%d", c);
-
-		if (i != 98)
+		printf("%d", i);
+		temp = num1 + num2;
+		num1 = num2;
+		num2 = temp;
+		if (count == 97)
 		{
+			total += num1 + num2;
+			printf("\n");
+		} else
+		{
+			total += num1;
 			printf(", ");
 		}
-
-		a = b;
-		b = c;
+		count++;
 	}
-
-	printf("\n");
 	return (0);
 }
