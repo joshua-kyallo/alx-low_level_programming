9de0c7d5ac5a493eb0b2408313a133d2024086d4
joshua-kyallo <Kyalllohjoshua@gmail.com>
2023-03-23 18:19:43 -0700

    More functions

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - test function
+ *
+ * @size: number of rows
+ *
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int i;
+	int j;
+	int k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size - 1; j++)
+				_putchar('#');
+			for (k = 0; k <= i; k++)
+				_putchar(' ');
+			_putchar('\n');
+		}
+	}
+}
