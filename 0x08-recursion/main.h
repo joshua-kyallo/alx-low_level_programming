acefc6c63cac6ff1bc70ea3841c74ef9cf779acd
joshua-kyallo <Kyalllohjoshua@gmail.com>
2023-04-05 16:05:08 -0700

    another commit

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,5 +5,7 @@ int _putchar(char c);
 void _puts_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
+int is_palindrome(char *s);
+int comparator(char *s, int n1, int n2);
 
 #endif
